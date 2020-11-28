d7dfe76374e9e52c8f57a322e41109de10d62a31
Emmanuel Ferte <eferte.omnilog@tf1.fr>
2020-11-28 14:34:57 +0100

    imrpove pages

--- js/main.js
+++ js/main.js
@@ -4,30 +4,30 @@ submenu.style.display = "none";
 
 document.querySelector(".menu-informatique").addEventListener("mouseenter", handlemouseover);
 function handlemouseover(event) {
-    submenu.style.display = "flex";
+  submenu.style.display = "flex";
 }
 
 window.addEventListener("mousemove", handlehidemenu);
 function handlehidemenu(event) {
-    let target = event.target;
-    if (target === menuinfo) {
+  let target = event.target;
+  if (target === menuinfo) {
+    return;
+  }
+  if (target === submenu) {
+    return;
+  }
+  if (target.parentNode) {
+    if (target.parentNode === submenu) {
+      return;
+    }
+    if (target.parentNode.parentNode) {
+      if (target.parentNode.parentNode === submenu) {
         return;
+      }
     }
-    if (target === submenu) {
-        return;
-    }
-    if (target.parentNode) {
-        if (target.parentNode === submenu) {
-            return;
-        }
-        if (target.parentNode.parentNode) {
-            if (target.parentNode.parentNode === submenu) {
-                return;
-            }
-        }
-    }
-   // console.log(event.target);
-    submenu.style.display = "none";
+  }
+  // console.log(event.target);
+  submenu.style.display = "none";
 }
 /*
 //on récupère l'élément mainMenu du DOM
@@ -42,3 +42,16 @@ link.innerHTML = '<img src="/img/logo3.jpg" height="100"/>'
 
 //inject cette image dans le DOM, juste avant le mainMenu
 parent.insertBefore(link, mainMenu);*/
+
+var burger = document.querySelector(".burger");
+burger.addEventListener("click", () => {
+  let menu = document.querySelector(".main-menu .menu");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+    submenu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+    submenu.style.display = "block";
+  }
+  
+});
